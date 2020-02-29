585d187ab340231c1eb9fa6df481a19ed1e1618a
ergouwang <mrergouwang@gmail.com>
2020-03-01 03:36:54 +0800

    第一个元素不需要 margin left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,39 +5,43 @@
 </template>
 
 <script>
-export  default {
-    mounted() {
-        //为了防止用户在 g-button 的外层再嵌套其他元素所进行的判断
-        for(let node of this.$el.children){
-            let name = node.nodeName.toLowerCase()
-            if(name !== 'button'){
-                console.warn(`g-button-group 的子元素应该全是 g-button,但是你写的是${name}`)
+    export default {
+        mounted() {
+            //为了防止用户在 g-button 的外层再嵌套其他元素所进行的判断
+            for (let node of this.$el.children) {
+                let name = node.nodeName.toLowerCase()
+                if (name !== 'button') {
+                    console.warn(`g-button-group 的子元素应该全是 g-button,但是你写的是${name}`)
+                }
             }
         }
     }
-}
 
 </script>
 
 <style lang="scss" scoped>
-    .g-button-group{
+    .g-button-group {
         display: inline-flex;
         vertical-align: middle;
 
-        > .g-button{
+        > .g-button {
             border-radius: 0;
-            margin-left: -1px;
 
-            &:first-child{
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+
+            &:first-child {
                 border-top-left-radius: var(--button-radius);
                 border-bottom-left-radius: var(--button-radius);
             }
 
-            &:last-child{
+            &:last-child {
                 border-top-right-radius: var(--button-radius);
                 border-bottom-right-radius: var(--button-radius);
             }
-            &:hover{
+
+            &:hover {
                 position: relative;
                 z-index: 1;
             }
