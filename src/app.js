a489d82e601f9f8cb06eb6610f20b55b8a65a2f4
ergouwang <mrergouwang@gmail.com>
2020-03-01 17:01:15 +0800

    使用 chai.expect 添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -11,6 +11,125 @@ new Vue({
     data:{
         loading1:false
     }
-
-
 })
+
+import  chai from 'chai'
+
+const expect = chai.expect
+
+//测试 settings
+{
+    //创建构造函数构造测试用例
+    const Constructor = Vue.extend(Button)
+    //创建测试用例
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    vm.$mount('#test')
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+}
+
+//测试loading
+{
+    //创建构造函数构造测试用例
+    const Constructor = Vue.extend(Button)
+    //创建测试用例
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+}
+//测试order
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    //创建构造函数构造测试用例
+    const Constructor = Vue.extend(Button)
+    //创建测试用例
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    // 断言完之后清楚内存
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试order
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    //创建构造函数构造测试用例
+    const Constructor = Vue.extend(Button)
+    //创建测试用例
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+
+}
+//测试click
+{
+    //创建构造函数构造测试用例
+    const Constructor = Vue.extend(Button)
+    //创建测试用例
+    const vm= new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+        }
+    })
+    vm.$mount()
+    vm.$on('click',function () {
+        console.log(1);
+    })
+    let button = vm.$el
+    button.click()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
