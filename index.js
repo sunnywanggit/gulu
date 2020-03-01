be55feb2ce0e25863fd7f24ab6cdb4abe2a194d4
ergouwang <mrergouwang@gmail.com>
2020-03-01 22:58:15 +0800

    完善 index.js 入口文件

---
+++ index.js
@@ -0,0 +1,7 @@
+//这个是你文件的入口，你需要在这个地方写清楚你需要导出哪些东西
+
+import Button from './src/button'
+import ButtonGroup from './src/button-group'
+import Icon from './src/components/icon'
+
+export {Button,ButtonGroup,Icon}
