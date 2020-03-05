065e0cfee250a0864571057d1b43f3db22b57989
ergouwang <mrergouwang@gmail.com>
2020-03-05 22:54:00 +0800

    基本的网格布局已成雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import Button from './button'
 import Icon from './components/icon'
 import buttonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', buttonGroup)
 Vue.component('s-input', Input)
+Vue.component('s-row',Row)
+Vue.component('s-col',Col)
+
 new Vue({
     el: '#app',
     data: {
---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col" :class="'span'+span">
+        <slot></slot>
+    </div>
+    
+</template>
+<script>
+    export default {
+        name:'s-col',
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col{
+        width:50%;
+        background: red;
+    }
+
+    @for $n from 1 through 24{
+        .span#{$n} {
+            width: ($n/24) * 100%;
+            background: red;
+        }
+    }
+
+    
+    
+    
+</style>
