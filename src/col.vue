96f4d8567525ed060ac333687956c9551c3b376e
ergouwang <mrergouwang@gmail.com>
2020-03-06 21:25:17 +0800

    重构 col.vue

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
     <!--    这种 class 的写法记下来-->
-    <div class="col" :class="[span && 'span'+span,offset && 'offset'+offset]"
-         :style="{marginLeft:gap/2+'px',marginRight:gap/2+'px'}"
-
-    >
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 
@@ -20,9 +17,22 @@
             },
 
         },
-        data(){
-            return{
-                gap:0
+        data() {
+            return {
+                gap: 0
+            }
+        },
+        computed: {
+            colStyle() {
+                return {
+                    marginLeft: this.gap / 2 + 'px',
+                    marginRight: this.gap / 2 + 'px'
+                }
+            },
+            colClass() {
+                //解构赋值，这个要好好学学
+                let {span, offset} = this
+                return [span && 'span' + span, offset && 'offset' + offset]
             }
         }
     }
