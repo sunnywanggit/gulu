941d1bf3aa7bf8b65c43a7c9b9ae7bec040f86e5
ergouwang <mrergouwang@gmail.com>
2020-03-17 22:59:22 +0800

    引入vuepress docs

---
+++ docs/.vuepress/components/demo-button.vue
@@ -0,0 +1,11 @@
+<template>
+    <s-button>默认按钮</s-button>
+</template>
+<script>
+    import Button from '../../../src/button.vue'
+    export default {
+        components:{
+            's-button':Button
+        }
+    }
+</script>
